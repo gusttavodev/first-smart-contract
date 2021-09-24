62788e3827823823ede8d2bba13e9f772d060068
gusttavodev <gustavo.silva@codificar.com.br>
2021-09-23 21:30:40 -0300

    first commit

---
+++ contracts/Storage.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.7.0 <0.9.0;
+
+contract SimpleStorage {
+  uint storageData; 
+  
+  function set(uint x) public {
+    storageData = x;
+  }
+
+  function get() public view returns (uint){
+    return storageData;
+  }
+
+}
